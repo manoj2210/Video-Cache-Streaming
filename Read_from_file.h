b80dd9e36aeafb5d343d1577b9c6f10ff957e82a
shruthi-22 <47136946+shruthi-22@users.noreply.github.com>
2019-04-15 04:50:01 -0700

    header for reading from file

---
+++ Read_from_file.h
@@ -0,0 +1,16 @@
+class Read_from_File
+{
+    private:
+        int no_videos;
+        int no_caches;
+        int no_endUsers;
+        int **lattency_user_from_cache;
+        int *lattency_user_from_dataserver;
+        int *mem_for_each_cache;
+        int *mem_for_each_video;
+        int **requests_by_videoId_times;
+        int requests;       //total no of requests placed by the users
+        int **request_details;
+    public:
+        void readData();
+};
