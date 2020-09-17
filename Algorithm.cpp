b82e5cbfa6d2699091b96d00bdfea60b40bed688
Manoj2210 <manojkumarmaths1000@gmail.com>
2020-09-17 17:18:06 +0530

    Add algorithm definition

--- Algorithm.cpp
+++ Algorithm.cpp
@@ -1,6 +1,63 @@
-#include"Algorithm.h"
+#include "Algorithm.h"
+#include<fstream>
 
-void Algorithm::WeightCalculator(LinkedListNode* temp)
+
+LinkedListNode* Algorithm::swap(LinkedListNode* ptr1, LinkedListNode* ptr2) 
+{ 
+    LinkedListNode* tmp = ptr2->link; 
+    ptr2->link = ptr1; 
+    ptr1->link = tmp; 
+    return ptr2; 
+}
+
+void Algorithm::sortLinkedList()
+{
+      /* Function to sort the list */
+    LinkedListNode **head=&list->Start;
+    int count=list->Sizeofll(); 
+
+    LinkedListNode **h; 
+    int i, j, swapped; 
+  
+    for (i = 0; i <= count; i++) { 
+  
+        h = head; 
+        swapped = 0; 
+  
+        for (j = 0; j < count - i - 1; j++) { 
+  
+            LinkedListNode *p1 = *h; 
+            LinkedListNode *p2 = p1->link; 
+  
+            if (p1->Sumofrequests < p2->Sumofrequests) { 
+  
+                /* update the link after swapping */
+                *h = swap(p1, p2); 
+                swapped = 1; 
+            } 
+  
+            h = &(*h)->link; 
+        } 
+  
+        /* break if the loop ended without any swap */
+        if (swapped == 0) 
+            break; 
+    } 
+    cout<<"Sorted"<<"\n\n";
+    list->Display();
+
+    LinkedListNode* temp=list->Start;
+    while(temp!=NULL)
+    {
+    WeightCalculator(temp);
+    InsertVideosinCaches(temp);
+    temp=temp->link;
+    }
+    PrintStack();
+} 
+
+
+void Algorithm::WeightCalculator(LinkedListNode * temp)
 {
     if(temp!=NULL)
     {
@@ -13,26 +70,25 @@ void Algorithm::WeightCalculator(LinkedListNode* temp)
             {   
                 if(Root->User[id]->link[j]->Remaingsize>=temp->Memory)
                 {
-                    Latency=Root->User[id]->link[j]->Lattencytoeachuser[i];
+                    Latency=Root->User[id]->link[j]->Lattencytoeachuser[id];
                     break;
                 }
-                else if(j=Numberofcacheservers-1)
+                else if(j==Numberofcacheservers-1)
                 {
                     Latency=Root->User[i]->LatencyfromDataServer;
                 }
             }
-            temp->Users[i].Weight=(float)(Latency/temp->Users[i].NumberofRequests);
         }
     }
-        
 }
 
 void Algorithm::InsertVideosinCaches(LinkedListNode *temp)
 {
+     
     if(temp!=NULL)
     {
         float min=temp->Users[0].Weight;
-        int id;
+        int id=temp->Users[0].Userid;
         for (unsigned int i = 0; i < temp->NumberofUsersofthisVideo; i++)
         {
             if(min>temp->Users[i].Weight)
@@ -44,17 +100,35 @@ void Algorithm::InsertVideosinCaches(LinkedListNode *temp)
             }
         }
 
-        for (unsigned j = 0; j < Numberofcacheservers; j++)
+        for (unsigned int j = 0; j < Numberofcacheservers; j++)
         {   
             if(Root->User[id]->link[j]->Remaingsize>=temp->Memory)
             {
                 Root->User[id]->link[j]->cacheVideos.push(Root->DataServervideos[temp->Videoid]);
-                Root->User[id]->link[j]->Remaingsize-=temp->Memory;
+                Root->User[id]->link[j]->Remaingsize=Root->User[id]->link[j]->Remaingsize-temp->Memory;
                 break;
             }
         }
     }
-
 }
 
+void Algorithm::PrintStack()
+{
+    ofstream fout;
+    fout.open("Outputfile.txt",ios::out);
 
+    fout<<"CacheId\t"<<"VideoId\t"<<"VideoSize\t"<<"No. of Elements\t"<<"RemainingSize\n";
+    
+    for(int i=0;i<Numberofcacheservers;i++)
+    {
+        for (stack<Video> dump = Root->Cacheserver[i]->cacheVideos; !dump.empty(); dump.pop())
+            {
+                fout<<i<<"\t"<<dump.top().id<<"\t"<<dump.top().Size<<"\t\t"<<Root->Cacheserver[i]->cacheVideos.size()<<"\t\t"<<Root->Cacheserver[i]->Remaingsize<<"\n";
+                cout << "id"<<dump.top().id << '\n';
+                cout << "size"<<dump.top().Size<< '\n';
+                cout << "(" << Root->Cacheserver[i]->cacheVideos.size() << " elements)\n";
+                cout<<"rs"<<Root->Cacheserver[i]->Remaingsize<<endl;
+            }
+    }
+    fout.close();
+}
