b2d2fcde18647c68b6fb9b00af36e81702aa0d9c
Manoj2210 <manojkumarmaths1000@gmail.com>
2020-09-17 17:23:17 +0530

    Update main: use algorithm as base structure

--- main.cpp
+++ main.cpp
@@ -1,21 +1,15 @@
 #include<iostream>
-// #include"DataServer.cpp"
+#include "Algorithm.cpp"
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    // DataServer A(1,2,2);
-    // A.createTreeStructure();
-    // A.temp();
-    // return 0;
-
-    UserRequests *user=new UserRequests;
-    user->NumberofRequests=1000;
-    user->Userid=100;
-
-    LinkedList list(1,2,user);
-    list.insertNode(5,6,user);
-    list.Display();
-
+    Algorithm A;
+    A.createTreeStructure();
+    A.temp();
+    A.constructLinkedList();
+    A.sortLinkedList();
+    return 0;
+    
 }
