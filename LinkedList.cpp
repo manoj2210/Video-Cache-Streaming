13583ffe8eaea4a353034fc3a9f14a432206e293
Manoj2210 <manojkumarmaths1000@gmail.com>
2020-09-17 17:20:56 +0530

    Add custom linked list definition

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,18 +1,19 @@
 #include"LinkedList.h"
 
-LinkedList::LinkedList(int Videoid,int Memory,UserRequests *Users)
+LinkedList::LinkedList(int Videoid,int Memory,UserRequests *Users,int Numberofrequests,int Total)
 {
     Start = new LinkedListNode;
 
     Start->Memory=Memory;
     Start->Videoid=Videoid;
     Start->Users = Users;
+    Start->Sumofrequests=Numberofrequests;
+    Start->NumberofUsersofthisVideo=Total;
     Start->link=NULL;
-
     ptr=Start;
 }
 
-void LinkedList::insertNode(int Videoid,int Memory,UserRequests *Users)
+void LinkedList::insertNode(int Videoid,int Memory,UserRequests *Users,int Numberofrequests,int Total)
 {
 
     LinkedListNode *temp = new LinkedListNode;
@@ -20,10 +21,24 @@ void LinkedList::insertNode(int Videoid,int Memory,UserRequests *Users)
     temp->Memory=Memory;
     temp->Videoid=Videoid;
     temp->Users = Users;
+    temp->Sumofrequests=Numberofrequests;
+    temp->NumberofUsersofthisVideo=Total;
     temp->link=NULL;
-
     ptr->link=temp;
     ptr=ptr->link;
+}
+int LinkedList::Sizeofll()
+{
+    LinkedListNode *temp;
+
+    temp=Start;
+    int count=0;
+    while(temp!=NULL)
+    {
+        count+=1;
+        temp=temp->link;
+    }
+    return count;
 }
 void LinkedList::Display()
 {
@@ -35,8 +50,11 @@ void LinkedList::Display()
     {
         cout<<"Memory"<<temp->Memory<<endl;
         cout<<"VideoID"<<temp->Videoid<<endl;
-        cout<<"UserID"<<temp->Users->Userid<<endl;
-        cout<<"NumberofRequest"<<temp->Users->NumberofRequests<<endl;
+        cout<<"Total Number of Users"<<temp->NumberofUsersofthisVideo<<endl;
+        cout<<"Sum of requests"<<temp->Sumofrequests<<endl;
+        cout<<"UserID"<<temp->Users[0].Userid<<endl;
+        cout<<"NumberofRequest"<<temp->Users[0].NumberofRequests<<endl;
+        
         temp=temp->link;
     }
 } 
