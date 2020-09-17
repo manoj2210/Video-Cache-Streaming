66f50ebb35c3f38218ab803de4ca5285ed3f11ac
Manoj2210 <manojkumarmaths1000@gmail.com>
2020-09-17 17:17:52 +0530

    Add algorithm file declaration

--- Algorithm.h
+++ Algorithm.h
@@ -1,15 +1,13 @@
-#include"DataServer.h"
+#include "DataServer.cpp"
 
 class Algorithm:public DataServer
 {
 
 public:
-
+    void sortLinkedList();
     void WeightCalculator(LinkedListNode*);            //Weight=Time/NumberofVideos
-
     void InsertVideosinCaches(LinkedListNode*);
-
-
-
+    LinkedListNode* swap(LinkedListNode* ptr1, LinkedListNode* ptr2) ;
+    void PrintStack();
 
 };
